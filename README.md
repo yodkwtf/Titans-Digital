b570db883bfebfb5f49c3359f716827ceb408e45
Durgesh Chaudhary <48durgesh.kumar@gmail.com>
2021-03-15 02:30:33 +0530

    project updated

---
+++ README.md
@@ -0,0 +1,7 @@
+## Titans Digital
+
+A website for an online web agency designed with html-css and javascipt. The design is completely original and built from scratch by me.
+
+#### Live Preview
+
+(https://titansdigital.netlify.app/)
--- app.js
+++ app.js
@@ -1,40 +1,36 @@
 // ********** set date ************
 // select span
 const date = (document.getElementById(
-    "date"
+  'date'
 ).innerHTML = new Date().getFullYear());
 
 // ********** nav toggle ************
 // select button and links
-const navBtn = document.getElementById("nav-toggle");
-const links = document.getElementById("nav-links");
+const navBtn = document.getElementById('nav-toggle');
+const links = document.getElementById('nav-links');
 // add event listener
-navBtn.addEventListener("click", () => {
-    links.classList.toggle("show-links");
+navBtn.addEventListener('click', () => {
+  links.classList.toggle('show-links');
 });
 
 // ********** smooth scroll ************
 // select links
-const scrollLinks = document.querySelectorAll(".scroll-link");
-scrollLinks.forEach(link => {
-    link.addEventListener("click", e => {
-        // prevent default
-        e.preventDefault();
-        links.classList.remove("show-links");
+const scrollLinks = document.querySelectorAll('.scroll-link');
+scrollLinks.forEach((link) => {
+  link.addEventListener('click', (e) => {
+    // prevent default
+    e.preventDefault();
+    links.classList.remove('show-links');
 
-        const id = e.target.getAttribute("href").slice(1);
-        const element = document.getElementById(id);
-        //
-        console.log(element);
+    const id = e.target.getAttribute('href').slice(1);
+    const element = document.getElementById(id);
 
-        let position = element.offsetTop - 62;
-        console.log(position);
+    let position = element.offsetTop - 62;
 
-        window.scrollTo({
-            left: 0,
-            // top: element.offsetTop,
-            top: position,
-            behavior: "smooth"
-        });
+    window.scrollTo({
+      left: 0,
+      top: position,
+      behavior: 'smooth',
     });
+  });
 });
